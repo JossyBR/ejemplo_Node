19a41c82a1c64e87b8f31f6fd7fe5b37b1b93418
JossyBR <jossybr94@gmail.com>
2025-04-02 21:42:56 -0500

    Creando modelos y base de datos

--- server.js
+++ server.js
@@ -9,6 +9,25 @@ app.get('/', (req, res) => {
     res.send('¡Hola JB!')
 })
 
-app.listen(process.env.PORT, () => {
+
+//Cargar archivos de conexion de base de datos
+const sequelize = require('./src/models/dbconnection')
+
+//Sincronizar los modelos cn la base de datos
+require('./src/models/sync_tables')
+
+app.listen(process.env.PORT, async () => {
     console.log(process.env.BIENVENIDA, process.env.PORT);
+    try {
+
+        //Conectarse a la base de datos
+        await sequelize.authenticate();
+        console.log("Conexión establecida con éxito a la base de datos!!!")
+        
+        //Crea las tablas sino existen, y actualiza cambios
+        await sequelize.sync({alter: true})
+        console.log('Tablas sincronizadas')
+    } catch (error) {
+        console.error("Error conectado a la base de datos", error)
+    }
 })
